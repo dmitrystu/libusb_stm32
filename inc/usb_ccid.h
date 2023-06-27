3cf84fbb2d97c699041f4954c3eb85d9882084fc
Filipe Paz Rodrigues <kidbomb@gmail.com>
2023-06-27 14:35:57 -0700

    Update usb_ccid.h

--- inc/usb_ccid.h
+++ inc/usb_ccid.h
@@ -6,21 +6,13 @@
     extern "C" {
 #endif
 
-#define USB_CLASS_CCID                   0x0b    /**<\brief Communicational Device class */
-#define USB_DTYPE_FUNCTIONAL_DESC   0x21   /**<\brief Functonal descriptor.*/
-        
-#define ENDPOINT_DIR_IN                    0x80
-#define ENDPOINT_DIR_OUT                   0x00
-        
-#define INTERFACE_ID_CCID                 0
-
-#define CCID_IN_EPADDR       (ENDPOINT_DIR_IN  | 2)
-
-/** Endpoint address of the CCID data OUT endpoint, for host-to-device data transfers. */
-#define CCID_OUT_EPADDR      (ENDPOINT_DIR_OUT | 1)
+#define USB_CLASS_CCID                   0x0b    /**<\brief Smart Card Device class */
+#define USB_DTYPE_FUNCTIONAL_DESC   0x21   /**<\brief Funcitonal descriptor.*/
+#define USB_DTYPE_CCID_FUNCTIONAL   0x21   /**<\brief Functional descriptor.*/
 
 /** Endpoint size in bytes of the CCID data being sent between IN and OUT endpoints. */
 #define CCID_EPSIZE          64
+
 
 
 #define CCID_CURRENT_SPEC_RELEASE_NUMBER            0x0110
