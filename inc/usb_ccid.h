0f0a1e73880298c132c8e16006288b2f6acda108
Filipe Paz Rodrigues <kidbomb@gmail.com>
2023-06-29 11:43:33 -0700

    Update usb_ccid.h

--- inc/usb_ccid.h
+++ inc/usb_ccid.h
@@ -1,3 +1,18 @@
+/* This file is the part of the Lightweight USB device Stack for STM32 microcontrollers
+ * Reference: https://www.usb.org/sites/default/files/DWG_Smart-Card_CCID_Rev110.pdf
+ *
+ * Copyright ©2023 Filipe Rodrigues <filipepazrodrigues[at]gmail[dot]com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include <stdint.h>
 
 #ifndef _USB_CCID_H_
@@ -7,13 +22,7 @@
 #endif
 
 #define USB_CLASS_CCID                   0x0b    /**<\brief Smart Card Device class */
-#define USB_DTYPE_FUNCTIONAL_DESC   0x21   /**<\brief Funcitonal descriptor.*/
 #define USB_DTYPE_CCID_FUNCTIONAL   0x21   /**<\brief Functional descriptor.*/
-
-/** Endpoint size in bytes of the CCID data being sent between IN and OUT endpoints. */
-#define CCID_EPSIZE          64
-
-
 
 #define CCID_CURRENT_SPEC_RELEASE_NUMBER            0x0110
 #define CCID_VOLTAGESUPPORT_5V                      0
@@ -42,26 +51,26 @@ struct usb_ccid_descriptor {
     uint8_t bLength;                /**<\brief Size of the descriptor, in bytes.*/
     uint8_t bDescriptorType;        /**<\brief IAD descriptor */
 
-    uint16_t                CCID;
-    uint8_t                 MaxSlotIndex;
-    uint8_t                 VoltageSupport;
-    uint32_t                Protocols;
-    uint32_t                DefaultClock;
-    uint32_t                MaximumClock;
-    uint8_t                 NumClockSupported;
-    uint32_t                DataRate;
-    uint32_t                MaxDataRate;
-    uint8_t                 NumDataRatesSupported;
-    uint32_t                MaxIFSD;
-    uint32_t                SynchProtocols;
-    uint32_t                Mechanical;
-    uint32_t                Features;
-    uint32_t                MaxCCIDMessageLength;
-    uint8_t                 ClassGetResponse;
-    uint8_t                 ClassEnvelope;
-    uint16_t                LcdLayout;
-    uint8_t                 PINSupport;
-    uint8_t                 MaxCCIDBusySlots;
+    uint16_t                bcdCCID;
+    uint8_t                 bMaxSlotIndex;
+    uint8_t                 bVoltageSupport;
+    uint32_t                dwProtocols;
+    uint32_t                dwDefaultClock;
+    uint32_t                dwMaximumClock;
+    uint8_t                 bNumClockSupported;
+    uint32_t                dwDataRate;
+    uint32_t                dwMaxDataRate;
+    uint8_t                 bNumDataRatesSupported;
+    uint32_t                dwMaxIFSD;
+    uint32_t                dwSynchProtocols;
+    uint32_t                dwMechanical;
+    uint32_t                dwFeatures;
+    uint32_t                dwMaxCCIDMessageLength;
+    uint8_t                 bClassGetResponse;
+    uint8_t                 bClassEnvelope;
+    uint16_t                wLcdLayout;
+    uint8_t                 bPINSupport;
+    uint8_t                 bMaxCCIDBusySlots;
 } __attribute__((packed));
 
 #ifdef __cplusplus
