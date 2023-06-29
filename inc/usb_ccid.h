a089062ba9b789aca50ff10450b0f21a64d136d4
Filipe Paz Rodrigues <kidbomb@gmail.com>
2023-06-29 12:36:09 -0700

    Update usb_ccid.h

--- inc/usb_ccid.h
+++ inc/usb_ccid.h
@@ -47,6 +47,34 @@
 #define CCID_GET_CLOCK_FREQUENCIES                  0x2
 #define CCID_GET_DATA_RATES                         0x3
 
+/* Bulk-OUT Messages */
+#define PC_TO_RDR_ICCPOWERON                   0x62
+#define PC_TO_RDR_ICCPOWEROFF                  0x63
+#define PC_TO_RDR_GETSLOTSTATUS                0x65
+#define PC_TO_RDR_XFRBLOCK                     0x6F
+#define PC_TO_RDR_GETPARAMETERS                0x6C
+#define PC_TO_RDR_RESETPARAMETERS              0x6D
+#define PC_TO_RDR_SETPARAMETERS                0x61
+#define PC_TO_RDR_ESCAPE                       0x6B
+#define PC_TO_RDR_ICCCLOCK                     0x6E
+#define PC_TO_RDR_T0APDU                       0x6A
+#define PC_TO_RDR_SECURE                       0x69
+#define PC_TO_RDR_MECHANICAL                   0x71
+#define PC_TO_RDR_ABORT                        0x72
+#define PC_TO_RDR_SETDATARATEANDCLOCKFREQUENCY 0x73
+
+/* Bulk-IN Messages */
+#define RDR_TO_PC_DATABLOCK                    0x80
+#define RDR_TO_PC_SLOTSTATUS                   0x81
+#define RDR_TO_PC_PARAMETERS                   0x82
+#define RDR_TO_PC_ESCAPE                       0x83
+#define RDR_TO_PC_DATARATEANDCLOCKFREQUENCY    0x84
+
+/* Interrupt-IN Messages */
+#define RDR_TO_PC_NOTIFYSLOTCHANGE             0x50
+#define RDR_TO_PC_HARDWAREERROR                0x51
+
+
 struct usb_ccid_descriptor {
     uint8_t bLength;                /**<\brief Size of the descriptor, in bytes.*/
     uint8_t bDescriptorType;        /**<\brief IAD descriptor */
@@ -72,6 +100,260 @@ struct usb_ccid_descriptor {
     uint8_t                 bPINSupport;
     uint8_t                 bMaxCCIDBusySlots;
 } __attribute__((packed));
+
+
+/* Bulk-Out Messages*/
+
+struct PC_to_RDR_IccPowerOn {
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 bPowerSelect;
+    uint8_t                 abRFU[2];
+}  __attribute__((packed));
+
+struct PC_to_RDR_IccPowerOff {
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 abRFU[3];
+}  __attribute__((packed));
+
+struct PC_to_RDR_GetSlotStatus {
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 abRFU[3];
+}  __attribute__((packed));
+
+struct PC_to_RDR_XfrBlock {
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 bSeq;
+    uint8_t                 bBWI;
+    uint16_t                wLevelParameter;
+    uint8_t                 abData[0];
+}  __attribute__((packed));
+
+struct PC_to_RDR_GetParameters {
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 abRFU[3];
+}  __attribute__((packed));
+
+struct PC_to_RDR_ResetParameters {
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 abRFU[3];
+}  __attribute__((packed));
+
+struct PC_to_RDR_SetParameters_T0 {
+    //common
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 bProtocolNum;
+    uint8_t                 abRFU[2];
+
+    //specific for T=0
+    uint8_t                 bmFindexDindex;
+    uint8_t                 bmTCCKST0;
+    uint8_t                 bGuardTimeT0;
+    uint8_t                 bWaitingIntegerT0;
+    uint8_t                 bClockStop;
+}  __attribute__((packed));
+
+struct PC_to_RDR_SetParameters_T1 {
+    //common
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 bProtocolNum;
+    uint8_t                 abRFU[2];
+
+    //specific for T=1
+    uint8_t                 bmFindexDindex;
+    uint8_t                 bmTCCKST1;
+    uint8_t                 bGuardTimeT1;
+    uint8_t                 bmWaitingIntegersT1;
+    uint8_t                 bClockStop;
+    uint8_t                 bIFSC;
+    uint8_t                 bNadValue;
+}  __attribute__((packed));
+
+struct PC_to_RDR_Escape {
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 abRFU[3];
+    uint8_t                 abData[0];
+}  __attribute__((packed));
+
+struct PC_to_RDR_IccClock {
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 bClockCommand;
+    uint8_t                 abRFU[2];
+}  __attribute__((packed));
+
+struct PC_to_RDR_T0APDU {
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 bmChanges;
+    uint8_t                 bClassGetResponse;
+    uint8_t                 bClassEnvelope;
+}  __attribute__((packed));
+
+struct PC_to_RDR_Secure {
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 bBWI;
+    uint16_t                wLevelParameter;
+    uint8_t                 abData[0];
+}  __attribute__((packed));
+
+struct PC_to_RDR_Mechanical {
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 bFunction;
+    uint8_t                 abRFU[2];
+}  __attribute__((packed));
+
+struct PC_to_RDR_Abort {
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 abRFU[3];
+}  __attribute__((packed));
+
+struct PC_to_RDR_SetDataRateAndClockFrequency {
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 abRFU[3];
+    uint32_t                dwClockFrequency;
+    uint32_t                dwDataRate;
+}  __attribute__((packed));
+
+/* Bulk-In Messages*/
+
+struct RDR_to_PC_DataBlock {
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 bStatus;
+    uint8_t                 bError;
+    uint8_t                 bChainParameter;
+    uint8_t                 abData[0];
+}  __attribute__((packed));
+
+struct RDR_to_PC_SlotStatus {
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 bStatus;
+    uint8_t                 bError;
+    uint8_t                 bClockStatus;
+}  __attribute__((packed));
+
+struct RDR_to_PC_Parameters_T0 {
+    //common
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 bStatus;
+    uint8_t                 bError;
+    uint8_t                 bProtocolNum;
+
+    //specific for T=0
+    uint8_t                 bmFindexDindex;
+    uint8_t                 bmTCCKST0;
+    uint8_t                 bGuardTimeT0;
+    uint8_t                 bWaitingIntegerT0;
+    uint8_t                 bClockStop;
+}  __attribute__((packed));
+
+struct RDR_to_PC_Parameters_T1 {
+    //common
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 bStatus;
+    uint8_t                 bError;
+    uint8_t                 bProtocolNum;
+
+    //specific for T=1
+    uint8_t                 bmFindexDindex;
+    uint8_t                 bmTCCKST1;
+    uint8_t                 bGuardTimeT1;
+    uint8_t                 bmWaitingIntegersT1;
+    uint8_t                 bClockStop;
+    uint8_t                 bIFSC;
+    uint8_t                 bNadValue;
+}  __attribute__((packed));
+
+struct RDR_to_PC_Escape {
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 bStatus;
+    uint8_t                 bError;
+    uint8_t                 bRFU;
+    uint8_t                 abData[0];
+}  __attribute__((packed));
+
+struct RDR_to_PC_DataRateAndClockFrequency {
+    uint8_t                 bMessageType;
+    uint32_t                dwLength;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 bStatus;
+    uint8_t                 bError;
+    uint8_t                 bRFU;
+    uint32_t                dwClockFrequency;
+    uint32_t                dwDataRate;
+}  __attribute__((packed));
+
+/* Interrupt-In Messages*/
+
+struct RDR_to_PC_NotifySlotChange {
+    uint8_t                 bMessageType;
+    uint8_t                 bmSlotICCState[0];
+}  __attribute__((packed));
+
+struct RDR_to_PC_NotifySlotChange {
+    uint8_t                 bMessageType;
+    uint8_t                 bSlot;
+    uint8_t                 bSeq;
+    uint8_t                 bHardwareErrorCode;
+}  __attribute__((packed));
 
 #ifdef __cplusplus
     }
